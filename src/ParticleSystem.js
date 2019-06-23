ae50de6bc1aa3202826832a4b974d8ceb4e634f5
David Norton <david.k.norton@gmail.com>
2019-06-23 09:17:25 -0500

    A demo I cooked up for testing my particle system performance

---
+++ src/ParticleSystem.js
@@ -0,0 +1,102 @@
+"use strict";
+class Particle
+{
+  constructor(pos,color,size,ttl)
+  {
+      this.vel = createVector(random(-1, 1), random(-1, 1));
+      this.pos = pos.copy();
+      this.ttl = 90;
+      this.color = 255;
+
+      if(color)
+      {
+          this.color = color;
+      }
+      if(size)
+      {
+        this.size = size;
+      }
+      if(ttl)
+      {
+        this.ttl = ttl;
+      }
+      let ttlVariation = 10
+      this.ttl = this.ttl + random(-ttlVariation, ttlVariation) //slight variation in particle lifetime
+  }
+
+  update()
+  {
+    if(this.ttl > 0)
+    {
+        this.ttl--;
+        this.pos.add(this.vel);
+
+        if(!onCanvas(this.pos.x,this.pos.y))
+        {
+            this.ttl=0;
+        }
+    }
+  }
+
+  //render with the size at the position
+  render()
+  {
+    if(this.ttl > 0)
+    {
+      stroke(this.color);
+      strokeWeight(this.size);
+      point(this.pos.x,this.pos.y);
+    }
+  }
+}
+
+class ParticleSystem
+{
+    constructor()
+    {
+        this._size = 10000
+        this._particles =  Array(this._size).fill(null);
+        this._nextPos = 0;
+        this.countParticles = true;
+        this.particleCount = 0;
+    }
+
+    addParticle(pos,color,size,ttl)
+    {
+      this._particles[this._nextPos] = new Particle(pos,color,size,ttl);
+      this._nextPos++;
+      if(this._nextPos >= this._size)
+      {
+        this._nextPos = 0;
+      }
+    }
+
+    addParticleSpray(pos,color,size,ttl,count)
+    {
+        if(count)
+        {
+            for(let i=0;i<count;i++)
+            {
+                this.addParticle(pos,color,size,ttl);
+            }
+        }
+    }
+
+    run()
+    {
+       this.particleCount=0;
+       for(let i = 0; i < this._particles.length; i++)
+       {
+         let p = this._particles[i];
+         if(p != null)
+         {
+           p.update();
+           p.render();
+           if(this.countParticles && p.ttl > 0)
+           {
+               this.particleCount++;
+           }
+         }
+       }
+    }
+}
---
+++ src/main.js
@@ -0,0 +1,205 @@
+"use strict";
+const debugMode = false;
+const frameDebug = false;
+const targetFrameRate = 60;
+const backgroundColor = 0;
+
+let points_string = '';
+let points_string_location;
+var FPS_string  = '';
+var FPS_string_location;
+
+
+let Global = {};
+Global.canvasWidth = 700;
+Global.canvasHeight = 700;
+Global.points = 0;
+Global.particleColor
+Global.particleSpamRate = 10
+
+function reset() {
+    let canvas = createCanvas(Global.canvasWidth, Global.canvasHeight);
+    canvas.parent('sketch-holder');
+    canvas.drawingContext.imageSmoothingEnabled = false;
+
+    frameRate(targetFrameRate);
+    background(0);
+
+    Global.textColor = color(255);
+
+    textSize(14);
+    textStyle(NORMAL);
+    textFont('Courier New');
+    stroke(color(255));
+    fill(color(255));
+
+    Global.points = 0;
+
+    Global.particleColor = color(0,255,0);
+    Global.ParticleSystem = new ParticleSystem();
+    
+    points_string_location = createVector(Global.canvasWidth*(19/24),20);
+    FPS_string_location = createVector(10,20);
+}
+
+function preload()
+{
+
+}
+
+function setup() {
+  reset();
+  setInterval(halfSecondUpdateLoop,500);
+}
+
+function draw() {
+
+    handleUserInput();
+
+    //BACKGROUND
+    background(backgroundColor); //black color
+
+    Global.ParticleSystem.run();
+    
+    renderBackgroundUI()
+}
+
+
+function mousePressed()
+{
+    let newPos = createVector(mouseX,mouseY);
+    Global.ParticleSystem.addParticleSpray(newPos,Global.particleColor,3,600,Global.particleSpamRate)
+}
+
+//handles continuous presses
+var handleUserInput = function()
+{
+  if(keyIsDown(32)/*space*/ || mouseIsPressed)
+  {
+
+  }
+};
+
+function keyPressed() {
+  if(keyCode == ENTER || keyCode == RETURN)
+  {
+    reset();
+    return;
+  }
+
+  //resumeSoundIfContextBlocked();
+
+  if(key == 'A' )
+  {
+      Global.particleSpamRate = 10
+  }
+
+  if(key == 'S')
+  {
+    Global.particleSpamRate = 100
+  }
+  
+  if(key == 'D')
+  {
+    Global.particleSpamRate = 1000
+  } 
+
+};
+
+function randomFromInterval(min,max){
+    return Math.random()*(max-min+1)+min;
+}
+
+//true-false coin-flip
+function coinFlip()
+{
+  return (int(Math.random() * 2) == 0);
+}
+
+function updateUIstuff()
+{
+  var fps = frameRate();
+  FPS_string = "FPS:" + fps.toFixed(0);
+
+  points_string = "Particles: " + Global.ParticleSystem.particleCount;
+}
+
+function renderBackgroundUI()
+{
+    textSize(14);
+    textStyle(NORMAL);
+    textFont('Courier New');
+    stroke(Global.textColor);
+    fill(Global.textColor);
+    text(FPS_string, FPS_string_location.x,FPS_string_location.y);
+    text(points_string,points_string_location.x,points_string_location.y);
+}
+
+function halfSecondUpdateLoop(){
+  updateUIstuff();
+}
+
+function preFillBackgroundStars()
+{
+  while(Global.backgroundStars.length < backgroundStarCount)
+  {
+    Global.backgroundStars.push(new BackgroundStar());
+  }
+}
+
+function onCanvas(x,y)
+{
+  if(x<0 || x > Global.canvasWidth)
+  {
+    return false;
+  }
+  if(y<0 || y > Global.canvasHeight)
+  {
+    return false;
+  }
+  return true;
+}
+ 
+
+function isABrightColor(color)
+{
+  let minColor = 100;
+  return (red(color)+green(color)+blue(color))>minColor;
+}
+
+ 
+
+ 
+
+function midpoint(x1,y1,x2,y2)
+{
+    return pointOnLine(x1,y1,x2,y2,0.5);
+}
+
+function pointOnLine(x1,y1,x2,y2,fraction)
+{
+    let newpoint = {x:(x1+x2)*fraction,y:(y1+y2)*fraction}
+    return newpoint;
+}
+
+ 
+ 
+
+function resumeSoundIfContextBlocked()
+{
+  if (getAudioContext().state !== 'running')
+  {
+        getAudioContext().resume();
+  }
+}
+
+function renderBackgroundUI()
+{
+    textSize(14);
+    textStyle(NORMAL);
+    textFont('Courier New');
+    stroke(Global.textColor);
+    fill(Global.textColor);
+    text(FPS_string, FPS_string_location.x,FPS_string_location.y);
+    text(points_string,points_string_location.x,points_string_location.y);
+}
